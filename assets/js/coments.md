1cbfd165d5b27ffff85436d849ffe4a543b455af
maria eduarda fantuci <mariafantuci@gmail.com>
2021-05-20 21:11:50 -0300

    manipulando Strings separação de frase

--- assets/js/coments.md
+++ assets/js/coments.md
@@ -42,3 +42,13 @@ console.log(phrase.toUpperCase())
 console.log(phrase.toLowerCas())
 //ou
 console.log(phrase.toLowerCase().toUpperCase())
+
+
+// manipulando string e arrays
+
+//perare um texto que contem espaços, em um novo array onde cada texto é uma posição do array. Depois disso, tranforme o array em um texto e onde eram espaços, coloque _
+
+let phrase = "I don't wanna live forever"
+let myArray = phrase.split(" ") // vai separa por espaço 
+
+let phraseWithUnderscore = myArray.join("_") // o método join junta os indexes do array, e o argumento o separador 
--- assets/js/teste.js
+++ assets/js/teste.js
@@ -1,10 +1,8 @@
-// Manipulando string e Números
+// manipulando string e arrays
 
-// transforme letras em maiusculas em minusculas. e fazendo o oposto
+//perare um texto que contem espaços, em um novo array onde cada texto é uma posição do array. Depois disso, tranforme o array em um texto e onde eram espaços, coloque _
 
-let phrase = "When tomorrow Come";
-console.log(phrase.toUpperCase())
-console.log(phrase.toLowerCas())
-//ou
-console.log(phrase.toLowerCase().toUpperCase())
+let phrase = "I don't wanna live forever"
+let myArray = phrase.split(" ") // vai separa por espaço 
 
+let phraseWithUnderscore = myArray.join("_") // o método join junta os indexes do array, e o argumento o separador 
