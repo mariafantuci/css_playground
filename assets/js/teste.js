3437722e95dd70eb75db6e1847108084edb25395
maria eduarda fantuci <mariafantuci@gmail.com>
2021-05-20 21:17:27 -0300

    achar texto específico

--- assets/js/teste.js
+++ assets/js/teste.js
@@ -1,8 +1,4 @@
-// manipulando string e arrays
+// verificar se o texto contém uma palavra específica
 
-//perare um texto que contem espaços, em um novo array onde cada texto é uma posição do array. Depois disso, tranforme o array em um texto e onde eram espaços, coloque _
-
-let phrase = "I don't wanna live forever"
-let myArray = phrase.split(" ") // vai separa por espaço 
-
-let phraseWithUnderscore = myArray.join("_") // o método join junta os indexes do array, e o argumento o separador 
+let phrase = "I just wanna live"
+console.log(phrase.includes("amor")) //o método includes verifica se tem em uma frase, ela é case sensitive
